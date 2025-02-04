1864d5b98908cfd4c6c20d621263f946026545ad
João Gabriel Muniz da Silva <infraestrutura@d2ti.com.br>
2025-02-04 15:23:45 -0300

    implementando Parser

---
+++ src/test/java/br/ufma/ecp/AppTest.java
@@ -0,0 +1,12 @@
+package br.ufma.ecp;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+public class AppTest {
+    @Test
+    public void test()
+    {
+        assertTrue( true );
+    }
+}
